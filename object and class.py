12bfe3ebc54543c1c7cd92a3d3ccc03b93a49adc
Shani Tiwari <shanitiwari3354@gmail.com>
2026-02-19 10:44:32 +0530

    Add student class with attributes and print details

---
+++ object and class.py
@@ -0,0 +1,11 @@
+class student:
+    name = "Hero"
+    roll_no = 21
+    marks = 89
+
+
+stu = student()
+print("Student Details")
+print(f"Name:{stu.name}")
+print(f"Roll No:{stu.roll_no}")
+print(f"Marks:{stu.marks}")
